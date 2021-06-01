156bc381f55144be1c7040c1b0b55bab75e18e36
Parsoa Khorsand <parsoakhorsand91@gmail.com>
2021-05-31 18:01:18 -0700

    Fix a bug with parse_sfsfile.

--- sfsutils.cpp
+++ sfsutils.cpp
@@ -7,22 +7,23 @@ map<string, vector<SFS>> parse_sfsfile(const string &sfs_path, int tau) {
     string line;
     ifstream inf(sfs_path);
     if (inf.is_open()) {
-        string info[5];
+        string info[4];
         string read_name;
         while (getline(inf, line)) {
             stringstream ssin(line);
             int i = 0;
-            while (ssin.good() && i < 6) {
+            while (ssin.good() && i < 5) {
                 ssin >> info[i++];
             }
-            if (stoi(info[4]) < tau) {
+            if (stoi(info[3]) < tau) {
                 continue;
             }
             if (info[0].compare("*") != 0) {
                 read_name = info[0];
                 SFSs[read_name] = vector<SFS>();
             }
-            SFSs[read_name].push_back(SFS(stoi(info[1]), stoi(info[2]), stoi(info[3]), info[4].compare("1") == 0));
+            //TODO
+            SFSs[read_name].push_back(SFS(stoi(info[1]), stoi(info[2]), stoi(info[3]), true)) ;//info[4].compare("1") == 0));
         }
     }
     return SFSs;
