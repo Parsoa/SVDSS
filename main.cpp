7489a8223e6fdb185a873b6d1c68b4b69ba43e7d
Luca Denti <ldenti@proton.me>
2025-05-12 11:18:42 +0200

    Wrap ropebwt3 build

--- main.cpp
+++ main.cpp
@@ -12,6 +12,10 @@
 
 using namespace std;
 
+extern "C" {
+int main_build(int argc, char *argv[]);
+}
+
 const string version = "v2.1.0";
 
 int main(int argc, char **argv) {
@@ -19,57 +23,62 @@ int main(int argc, char **argv) {
   time(&t);
   spdlog::set_default_logger(spdlog::stderr_color_st("stderr"));
 
-  auto c = Configuration::getInstance();
-
+  // Print help if no arguments
   if (argc == 1) {
-    c->print_help("");
+    cerr << MAIN_USAGE_MESSAGE << endl;
+    // c->print_help("");
     exit(EXIT_FAILURE);
   }
 
-  c->parse(argc, argv);
-  if (c->verbose)
-    spdlog::set_level(spdlog::level::debug);
-
-  if (c->version) {
-    cout << "SVDSS, " << version << endl;
-    exit(EXIT_SUCCESS);
-  }
-
-  if (c->help) {
-    c->print_help(argv[1]);
-    exit(EXIT_SUCCESS);
-  }
-
-  if (strcmp(argv[1], "call") == 0) {
-    if (c->reference == "" || c->bam == "" || c->sfs == "") {
-      c->print_help(argv[1]);
-      exit(EXIT_FAILURE);
-    }
-    auto caller = new Caller();
-    caller->run();
-  } else if (strcmp(argv[1], "index") == 0) {
-    spdlog::critical("Please use robebwt3 to create the index");
-    spdlog::critical("e.g., ropebwt3 build -t16 -d <REFERENCE.FA> > <REFERENCE.FA.FMD>");
-    exit(EXIT_FAILURE);
-  } else if (strcmp(argv[1], "search") == 0) {
-    if (c->index == "" || (c->fastq == "" && c->bam == "")) {
-      c->print_help(argv[1]);
-      exit(EXIT_FAILURE);
-    }
-    auto pingpong = new PingPong();
-    pingpong->search();
-    // } else if (strcmp(argv[1], "assemble") == 0) {
-    //   auto assembler = new Assembler();
-    //   assembler->run();
-  } else if (strcmp(argv[1], "smooth") == 0) {
-    if (c->reference == "" || c->bam == "") {
-      c->print_help(argv[1]);
-      exit(EXIT_FAILURE);
-    }
-    Smoother().run();
+  // Run ropebwt3 build if index mode
+  if (strcmp(argv[1], "index") == 0) {
+    spdlog::info(
+        "We rely on 'ropebwt3 build' - just exposing it for convenience");
+    main_build(argc - 1, argv + 1);
   } else {
-    c->print_help("");
-    exit(EXIT_FAILURE);
+    // Get configuration and run
+    auto c = Configuration::getInstance();
+    c->parse(argc, argv);
+    if (c->verbose)
+      spdlog::set_level(spdlog::level::debug);
+
+    if (c->version) {
+      cout << "SVDSS, " << version << endl;
+      exit(EXIT_SUCCESS);
+    }
+
+    if (c->help) {
+      c->print_help(argv[1]);
+      exit(EXIT_SUCCESS);
+    }
+
+    if (strcmp(argv[1], "call") == 0) {
+      if (c->reference == "" || c->bam == "" || c->sfs == "") {
+        c->print_help(argv[1]);
+        exit(EXIT_FAILURE);
+      }
+      auto caller = new Caller();
+      caller->run();
+    } else if (strcmp(argv[1], "search") == 0) {
+      if (c->index == "" || (c->fastq == "" && c->bam == "")) {
+        c->print_help(argv[1]);
+        exit(EXIT_FAILURE);
+      }
+      auto pingpong = new PingPong();
+      pingpong->search();
+      // } else if (strcmp(argv[1], "assemble") == 0) {
+      //   auto assembler = new Assembler();
+      //   assembler->run();
+    } else if (strcmp(argv[1], "smooth") == 0) {
+      if (c->reference == "" || c->bam == "") {
+        c->print_help(argv[1]);
+        exit(EXIT_FAILURE);
+      }
+      Smoother().run();
+    } else {
+      c->print_help("");
+      exit(EXIT_FAILURE);
+    }
   }
   time_t s;
   time(&s);
